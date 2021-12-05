917ff24bb97470ec0ce87177005271dd2fab2c99
AX03-sudo <ahmadax03@gmail.com>
2021-12-05 14:47:28 +0300

    Ahmad_Alsaffar

--- MainProgram.java
+++ MainProgram.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class MainProgram {
     static void main(String[] args){
-
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Input");
     }
 }
