079a6a368634b67ad811764f0bc67f7e4bdd2a9c
Vervain21 <95569384+Vervain21@users.noreply.github.com>
2021-12-06 13:21:12 +0300

    Update Login.java
    Aziz

--- Login.java
+++ Login.java
@@ -1,2 +1,13 @@
 public class Login {
+    public static int login(String username, String password) {
+
+        for (int i = 0; i < Main.usersArr.size(); i++) {
+            if (Main.usersArr.get(i).getUsername().equalsIgnoreCase(username)
+                    && Main.usersArr.get(i).getPassword().equals(password)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
