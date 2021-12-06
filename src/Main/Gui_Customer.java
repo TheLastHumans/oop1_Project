9aa1387e61e68b4540080dff43b6933ac042246e
AX03-sudo <ahmadax03@gmail.com>
2021-12-06 17:00:31 +0300

    Ahmad_Alsaffar

--- src/Main/Gui_Customer.java
+++ src/Main/Gui_Customer.java
@@ -1,15 +1,21 @@
 package Main;
 
-import javax.swing.*;
+import javax.swing.*;import java.awt.event.ComponentAdapter;
 
 public class Gui_Customer extends JFrame {
     private JPanel P_Customer;
+    private JList list1;
 
     public Gui_Customer() {
         setContentPane(P_Customer);
         setTitle("Customer View");
-        setSize(640,480);
+        setSize(640, 480);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(false);
+        String[] WoodType = {"cherry", "oak", "walnut", "maple", "pine", "akasa", "teak", "poplar", "redwood", "beech"};
+        list1 = new JList(WoodType);
+
+
+
     }
 }
