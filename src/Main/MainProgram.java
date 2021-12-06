8fc1d084e970a64a4ca70894ae0a58e3aa4a2c1f
AX03-sudo <ahmadax03@gmail.com>
2021-12-06 13:23:56 +0300

    Ahmad_Alsaffar

--- src/Main/MainProgram.java
+++ src/Main/MainProgram.java
@@ -2,8 +2,22 @@ package Main;
 
 import java.util.Scanner;
 public class MainProgram {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-        System.out.println("Input");
-    }
-}
+        int Login_Input = 0;
+        while (1==1) {
+            System.out.println("Login or continue as customer : \n1) login\n2) Continue as customer\nInput :" + Input.nextInt(Login_Input));
+            if (Login_Input == 1) {
+                System.out.println("line 1");
+                break;
+            }//if login 1
+            else if (Login_Input == 2) {
+                System.out.println("line 2");
+                break;
+            }//If Login 2
+            else {
+                System.out.println("Wrong Input");
+            }//else Login
+        }//While Main
+    }//Main Class
+}//MainProgram
