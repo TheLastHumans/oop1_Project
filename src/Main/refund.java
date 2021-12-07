7fed3a330dc38be2a92bf0c844d270f1298bdb89
AX03-sudo <ahmadax03@gmail.com>
2021-12-07 18:32:21 +0300

    Ahmad_Alsaffar

--- src/Main/refund.java
+++ src/Main/refund.java
@@ -1,4 +1,20 @@
 package Main;
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.contains;
 
 public class refund {
+    //int x;
+
+    public refund() {
+        //this.x = x;
+    }
+
+    public int[] return_item(int[] Orders_Int, int item_num) {
+        if(contains(Orders_Int, item_num)) { // yes
+            int[] result = {item_num,item_num-1};
+            return result;
+        } else {
+            int[] result = {0,0};
+            return result;
+        }
+    }
 }
